3757dda12c883282ee54eb4cc0f137b12920a416
phluoroGG <phluoro@gmail.com>
2022-03-08 18:12:01 +0300

    Create project

---
+++ main.py
@@ -0,0 +1,32 @@
+def read_string_from_file(filename):
+    file = open(filename, 'r', encoding='utf-8')
+    string = file.read()
+    file.close()
+    return string
+
+
+def reverse_words_symmetrical(string):
+    english_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    russian_alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    chars = [char for char in string]
+    for j in range(len(chars)):
+        if chars[j] == ' ':
+            continue
+        if english_alphabet.find(chars[j].lower()) + 1:
+            char = chars[j]
+            chars[j] = english_alphabet[len(english_alphabet) - english_alphabet.find(char.lower()) - 1]
+            if char.isupper():
+                chars[j] = chars[j].upper()
+        elif russian_alphabet.find(chars[j].lower()) + 1:
+            char = chars[j]
+            chars[j] = russian_alphabet[len(russian_alphabet) - russian_alphabet.find(char.lower()) - 1]
+            if char.isupper():
+                chars[j] = chars[j].upper()
+    return ''.join(chars)
+
+
+if __name__ == '__main__':
+    filename_ = "input01.txt"
+    reversed_string = reverse_words_symmetrical(read_string_from_file(filename_))
+    print(reversed_string)
+    print(reverse_words_symmetrical(reversed_string))
